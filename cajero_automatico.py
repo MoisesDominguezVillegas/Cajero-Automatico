ebb0d6ef222b3af087946c712d50024d0ba9d2ea
MoisesDominguezVillegas <druidax66x@gmail.com>
2024-06-04 20:51:26 +0200

    first commit

--- cajero_automatico.py
+++ cajero_automatico.py
@@ -10,3 +10,22 @@ while not salir:
     4. Salir''')
     opcion = int(input('Escoje una opcion: '))
 
+    if opcion == 1:
+        print(f'Tu saldo actual es: {saldo: .2f}€\n')
+    elif opcion == 2:
+        retiro = float(input('Ingresa la cantidad a retirar: '))
+        # Validacion
+        if retiro <= saldo:
+               saldo -= retiro # saldo = saldo - retiro
+               print(f'Tu nuevo saldo es: {saldo: .2f}€\n')
+        else:
+               print(f'No cuentas con el saldo suficiente, Tu saldo es: {saldo}')
+    elif opcion == 3:
+        deposito = float(input('Ingresa la cantidad a depositar: '))
+        saldo += deposito # saldo = saldo + deposito
+        print(f'Tu nuevo saldo es: {saldo: .2f}€\n')
+    elif opcion == 4:
+        print('Saliendo del cajero automatico, Hasta pronto!...')
+        salir = True
+    else:
+        print('Opcion invalida. Selecciona otra opcion')
